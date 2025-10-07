45881c24d4315fed3a69649e7442fae01a0e6715
Doreen2002 <doreenmwapekatebe8@gmail.com>
2025-10-07 11:35:00 +0200

    fix pull naming series from invoice to pos profile

--- pos_bahrain/hooks.py
+++ pos_bahrain/hooks.py
@@ -46,6 +46,7 @@ app_include_js = [
 
 # include js in doctype views
 doctype_js = {
+	"POS Profile": "public/js/pos_profile.js",
     "Mode of Payment": "public/js/mode_of_payment.js",
     "Stock Entry": ["public/js/includes/scan_barcode.js", "public/js/stock_entry.js"],
     "Company": "public/js/company.js",
