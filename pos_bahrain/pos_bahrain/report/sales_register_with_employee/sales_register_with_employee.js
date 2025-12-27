182d552613cdf6eab43fc1b72b853b218f1a56a8
Doreen2002 <doreenmwapekatebe8@gmail.com>
2025-12-27 16:18:33 +0200

    Fix filters Sales Register with Employee Report

--- pos_bahrain/pos_bahrain/report/sales_register_with_employee/sales_register_with_employee.js
+++ pos_bahrain/pos_bahrain/report/sales_register_with_employee/sales_register_with_employee.js
@@ -2,6 +2,32 @@
 // For license information, please see license.txt
 /* eslint-disable */
 
-frappe.query_reports[
-  'Sales Register with Employee'
-] = pos_bahrain.reports.sales_register_with_employee();
+
+frappe.query_reports["Sales Register with Employee"] = {
+  "filters": [],
+  "onload": async function (report) {
+      const source_report = "Sales Register";
+      if (!frappe.query_reports[source_report]) {
+          const base = new frappe.views.QueryReport();
+          base.report_name = source_report;
+          await base.get_report_doc();
+          await base.get_report_settings();
+      }
+      const custom_filters = [
+          {
+              fieldname: 'sales_employee',
+              label: __('Sales Employee'),
+              fieldtype: 'Link',
+              options: 'Employee',
+          },
+          {
+              fieldname: 'commission_rate',
+              label: __('Commission Rate (%)'),
+              fieldtype: 'Float',
+          }
+      ];
+      const original_filters = frappe.query_reports[source_report].filters || [];
+      report.report_settings.filters = [...original_filters, ...custom_filters];
+      report.setup_filters();
+  }
+};
