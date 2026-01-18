5fe79b3d62c479149c06ac5aacf6f37bf337d2b3
Doreen2002 <doreenmwapekatebe8@gmail.com>
2026-01-18 15:13:31 +0200

    Fix - create POS naming series fields

---
+++ pos_bahrain/public/js/pos_invoice.js
@@ -0,0 +1,17 @@
+frappe.ui.form.on('POS Invoice', {
+    pos_profile: function(frm) { set_series(frm); },
+    is_pos: function(frm) { 
+      setTimeout(() => { set_series(frm); }, 500); 
+    }
+  });
+  
+  function set_series(frm) {
+    if (frm.is_new() && frm.doc.pos_profile && frm.doc.is_pos) {
+      frappe.db.get_value('POS Profile', frm.doc.pos_profile, 'naming_series', (r) => {
+        if (r && r.naming_series) {
+          frm.set_value("naming_series", r.naming_series);
+          frm.refresh_field("naming_series"); 
+        }
+      });
+    }
+  }
