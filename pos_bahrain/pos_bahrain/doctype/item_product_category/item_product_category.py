ca9eb1405af48996da3917928a305913e6830871
Doreen2002 <doreenmwapekatebe8@gmail.com>
2026-02-03 15:15:44 +0200

    Add - Item Product Catergory

---
+++ pos_bahrain/pos_bahrain/doctype/item_product_category/item_product_category.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, 	9t9it and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class ItemProductCategory(Document):
+	pass
