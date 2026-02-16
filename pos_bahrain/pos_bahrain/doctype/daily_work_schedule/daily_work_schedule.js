0ee70be7f468e7b37bec51b8c82456ef0cc9c32a
Doreen2002 <doreenmwapekatebe8@gmail.com>
2026-02-16 14:42:37 +0200

    Add - Daily Work Schedule Doctype

---
+++ pos_bahrain/pos_bahrain/doctype/daily_work_schedule/daily_work_schedule.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, 	9t9it and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Daily Work Schedule", {
+// 	refresh(frm) {
+
+// 	},
+// });
